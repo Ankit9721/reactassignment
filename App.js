ec27d0f707dbde355291ca9de85e96d33693ba5f
Ankit9721 <ankitarya9580@gmail.com>
2023-07-12 12:21:31 +0530

    updated

---
+++ App.js
@@ -0,0 +1,53 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Provider as PaperProvider } from "react-native-paper";
+import { Provider } from "react-redux";
+import store from "./src/redux/store";
+import BudgetEntry from "./src/screens/BudgetEntry";
+import BudgetList from "./src/screens/BudgetList";
+import { FontAwesome } from "@expo/vector-icons";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <PaperProvider>
+          <Stack.Navigator initialRouteName="BudgetEntry">
+            <Stack.Screen
+              name="BudgetEntry"
+              component={BudgetEntry}
+              options={{
+                title: "Budget Entry",
+                headerLeft: () => (
+                  <FontAwesome
+                    style={{ margin: 10, color: "brown" }}
+                    name="book"
+                    size={28}
+                    color="black"
+                  />
+                ),
+              }}
+            />
+            <Stack.Screen
+              name="BudgetList"
+              component={BudgetList}
+              options={{
+                title: "Budget Entry Listing",
+                headerLeft: () => (
+                  <FontAwesome
+                    style={{ margin: 10, color: "brown" }}
+                    name="book"
+                    size={28}
+                    color="black"
+                  />
+                ),
+              }}
+            />
+          </Stack.Navigator>
+        </PaperProvider>
+      </NavigationContainer>
+    </Provider>
+  );
+}
